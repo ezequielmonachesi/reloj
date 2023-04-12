9ebca0c4cc4695d821e91c238181d352b0719583
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-04-12 10:51:52 -0300

    Se agregó am y pm para poder mostrar en la hora

--- js/app.js
+++ js/app.js
@@ -1,26 +1,59 @@
-function obtenerFecha(){
 
-    let fecha = document.querySelector('#fecha');
-    let hora = document.querySelector('#hora');
-let fechaAcutal = new Date();
+function obtenerFecha() {
+  let fecha = document.querySelector("#fecha");
+  let hora = document.querySelector("#hora");
+  let fechaAcutal = new Date();
 
-// Array de días de la semana
-const daysOfWeek = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-// Array de meses
-const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+  // Array de días de la semana
+  const daysOfWeek = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+  // Array de meses
+  const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
 
-fecha.innerHTML = `${daysOfWeek[fechaAcutal.getDay()]} ${fechaAcutal.getDate()} de ${months[fechaAcutal.getMonth()]} del ${fechaAcutal.getFullYear()}`;
-let segundos = 0;
-if(fechaAcutal.getSeconds()<10){
-    segundos ='0'+fechaAcutal.getSeconds();
-}else{
-    segundos =fechaAcutal.getSeconds();
+  fecha.innerHTML = `${
+    daysOfWeek[fechaAcutal.getDay()]
+  } ${fechaAcutal.getDate()} de ${
+    months[fechaAcutal.getMonth()]
+  } del ${fechaAcutal.getFullYear()}`;
+  let segundos = 0;
+  if (fechaAcutal.getSeconds() < 10) {
+    segundos = "0" + fechaAcutal.getSeconds();
+  } else {
+    segundos = fechaAcutal.getSeconds();
+  }
+  let hour = 0;
+  let ampm = "";
+  if(fechaAcutal.getHours() < 13){
+    hour = fechaAcutal.getHours();
+    ampm = "am"
+  }else if(fechaAcutal.getHours() % 12 === 0){
+    hour = fechaAcutal.getHours()%12;
+    ampm = "am"
+  }
+  else{
+    hour = fechaAcutal.getHours()%12;
+    ampm = "pm"
+  }
+  hora.innerHTML = `${hour}:${fechaAcutal.getMinutes()}:${segundos} ${ampm}`;
 }
-
-hora.innerHTML = `${fechaAcutal.getHours()}:${fechaAcutal.getMinutes()}:${segundos}`
-
-}
-setInterval(obtenerFecha, 1000)
-
-// Martes 11 de Abril del 2023
-// 10:35:00
+setInterval(obtenerFecha, 10);
